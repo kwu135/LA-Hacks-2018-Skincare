498d096d7b70c78191aa6c9a2336c59606643bbd
Alex Hong <hongalex@usc.edu>
2018-03-31 11:31:42 -0700

    updated product page formatting

--- Skincare/src/components/Product.vue
+++ Skincare/src/components/Product.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
-  	<b-container class="bv-row">
-  		<b-row>
-  			<b-col md="2" align-self="center">
-  				<b-img src="http://www.clker.com/cliparts/3/m/v/Y/E/V/small-red-apple-hi.png" fluid alt="Responsive Image" />
-  			</b-col>
-  			<b-col md="6">
-			    	<h1>{{ title }}</h1>
-			    	<p>
-				    	<b-list-group v-for="ingredient in ingredients">
-				    		<b-list-group-item>{{ ingredient }}</b-list-group-item>
-				    	</b-list-group>
-			    	</p>
-			</b-col>
-			<b-col md="4" align-self="center">
-				<b-button class="button" variant='danger' @click="badProduct()">Product does not work for me!</b-button>
-				<b-button class="button" variant='danger' @click="removeProduct()">Remove Product </b-button>
-			</b-col>
-		</b-row>
-	</b-container>
+    <b-container class="bv-row">
+      <b-row class="justify-content-md-center">
+        <b-col md="6">
+            <h1>{{ name }}</h1>
+            <p>
+              <h5> Ingredients </h5>
+              <b-list-group v-for="ingredient in ingredients">
+                <b-list-group-item>{{ ingredient.name }}</b-list-group-item>
+              </b-list-group>
+            </p>
+        </b-col>
+        <b-col md="4">
+          <b-img src="http://via.placeholder.com/300x200" fluid alt="Responsive Image" />
+
+          <b-button class="button" variant='primary' v-if="!userOwnsProduct" @click="addProduct()">I own this product</b-button>
+          <b-button class="button" variant='danger' v-else @click="removeProduct()">I don't own this product</b-button>
+
+          <b-button class="button" variant='danger' v-if="!userFlaggedProduct" @click="flagBadProduct()">
+            Product doesn't work for me!
+          </b-button>
+          <b-button class="button" variant='primary' v-else @click="unflagBadProduct()">
+            Unflag bad product
+          </b-button>          
+
+        </b-col>
+    </b-row>
+  </b-container>
   </div>
 </template>
 
@@ -27,35 +35,54 @@ export default {
   name: 'Product',
   data () {
     return {
-      title: 'Product ' + this.$route.params.product,
-      ingredients: {
-    	abc: 'Hydraulic Acid',
-    	def: 'Tea Tree Oil',
-    	xyz: 'Glycolic Acid',
-    	ddd: 'Almond Oil'
-   	  }
+      name: this.$route.params.product,
+      ingredients: [
+      {
+        name: 'Hydraulic Acid',
+      },
+      {
+        name: 'Tea Tree Oils'
+      },
+      {
+        name: 'Glycolic Acid'
+      },
+      {
+        name: 'Almond Oil'
+      }],
+      userOwnsProduct: false,
+      userFlaggedProduct: false
     }
     
   },
   methods: {
-  	badProduct(){
-  		//add badproduct method
-  	},
-  	removeProduct(){
-  		//add removeproduct method
-  		
-  	}
+    addProduct() {
+      this.userOwnsProduct = true;
+    },
+    removeProduct() {
+      this.userOwnsProduct = false;
+      //add removeproduct method
+    },
+    flagBadProduct() {
+      this.userFlaggedProduct = true;
+    },
+    unflagBadProduct() {
+      this.userFlaggedProduct = false;
+    }
   }
 }
 </script>
 
 <style>
-	h1 {
-		color: teal;
-	}
-	.button{
-		display: block;
-		margin: auto;
-		float: center;
-	}
+  h1 {
+    color: teal;
+  }
+
+  .img-fluid {
+    margin: 1em;
+  }
+
+  .button{
+    display: block;
+    margin: 1em auto;
+  }
 </style>
