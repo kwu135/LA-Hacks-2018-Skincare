f17af18b7e35ddb9e013c77e9919c32c16837b2b
Alex Hong <hongalex@usc.edu>
2018-03-31 03:44:47 -0700

    basic login page

--- Skincare/src/components/Login.vue
+++ Skincare/src/components/Login.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
+    <b-container class="bv-row">
+      <b-row>
+        <b-col md="4" offset-md="4">
+          <br>
+          <b-form>
+            <b-form-group id="emailGroup">
+              <b-form-input type="text" 
+                            id="email"
+                            v-model="credentials.email"
+                            required
+                            placeholder="Email">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="passGroup">
+              <b-form-input type="password" 
+                            id="username"
+                            v-model="credentials.password"
+                            required
+                            placeholder="Password">
+              </b-form-input>   
+            </b-form-group>
+            <b-button size="sm" variant="primary" @click="login()">
+              Login
+            </b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -9,7 +36,18 @@ export default {
   name: 'Login',
   data () {
     return {
-      title: 'Login here'
+      credentials: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      var credentials = {
+        email: this.credentials.email,
+        password: this.credentials.password
+      }
     }
   }
 }
