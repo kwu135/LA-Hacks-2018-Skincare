3edfe5eee06ec476b8a8f7ed618573bf15e2a9f6
Alex Hong <hongalex@usc.edu>
2018-03-31 03:16:42 -0700

    additional components

--- Skincare/src/components/Home.vue
+++ Skincare/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		{{msg}}
+		{{title}}
 	</div>
 </template>
 
@@ -9,26 +9,8 @@ export default {
 	name: 'Home',
 	data () {
 		return {
-			msg: 'Welcome to Your Vue.js App'
+			title: 'Default home page'
 		}
 	}
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-	font-weight: normal;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 0;
-}
-a {
-	color: #42b983;
-}
-</style>
---
+++ Skincare/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      title: 'Login here'
+    }
+  }
+}
+</script>
+
