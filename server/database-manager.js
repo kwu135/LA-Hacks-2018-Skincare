837b43624d85314ec9f49edf295a344d8bad4639
Kevin Wu <kwu135@usc.edu>
2018-04-01 11:27:05 -0700

    fix quotes

--- server/database-manager.js
+++ server/database-manager.js
@@ -534,7 +534,7 @@ class DatabaseManager
         this.validateSessionToken(email, sessionToken).then((tempUser) => {
             user = tempUser;
             if (!user) {
-                deferred.reject(“Invalid session token.“);
+                deferred.reject("Invalid session token.");
                 return;
             }
             var promises = [];
@@ -542,7 +542,7 @@ class DatabaseManager
             for (var i=0; i < user.flaggedProductList.length; i++) {
                 var product = user.flaggedProductList[i];
                 // Query product’s ingredients list
-                const query = datastore.createQuery(‘Product’).filter(‘name’, ‘=’, product);
+                const query = datastore.createQuery(‘Product’).filter('name', '=', product);
                 var promise = datastore.runQuery(query).then(results => {
                     return results[0][0].ingredients;
                 });
